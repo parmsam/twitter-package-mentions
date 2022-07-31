1f63d13440fd56a55943ca1dc1a4deb30341c239
parmsam <parmartsam@gmail.com>
2022-07-31 14:44:26 -0400

    use here instead

--- R/03_save.R
+++ R/03_save.R
@@ -1,3 +1,4 @@
 library(readr)
-write_rds(all_tweets, "data/all_tweets.RDS")
-write_rds(packageMentions, "data/packages.RDS")
+library(here)
+write_rds(all_tweets, here("data", "all_tweets.RDS"))
+write_rds(packageMentions, here("data", "packages.RDS"))
